4b6764b38cbaad3fff6dfb0df457aa5f57818f10
Dr. M A Abbas <62662314+adrish2001@users.noreply.github.com>
2026-05-27 13:31:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    // Get the first letter of the current name
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // Check if the first letter is 'j'
+    if (firstLetter === 'j') {
+      goodbyeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
